285002f457aa53a74c0f8a85f6c6513c57df4a45
skanehira <sho19921005@gmail.com>
2019-10-26 22:04:19 +0900

    move keybindings to keybindings.go

--- gui/gui.go
+++ gui/gui.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os/exec"
 
-	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
@@ -86,95 +85,7 @@ func (gui *Gui) Run() error {
 	gui.HistoryManager.Save(0, currentDir)
 	gui.EntryManager.SetEntries(currentDir)
 
-	gui.EntryManager.SetDoneFunc(func(key tcell.Key) {
-		if key == tcell.KeyEscape {
-			gui.App.Stop()
-		}
-
-	}).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		globalKeybinding(gui, event)
-		switch {
-		// cut entry
-		case event.Rune() == 'd':
-			if !hasEntry(gui) {
-				return event
-			}
-			//gui.EntryManager.SetViewable(false)
-			gui.Register.MoveSources = append(gui.Register.MoveSources, gui.EntryManager.GetSelectEntry())
-
-		// copy entry
-		case event.Rune() == 'y':
-			if !hasEntry(gui) {
-				return event
-			}
-			gui.Register.CopySources = append(gui.Register.CopySources, gui.EntryManager.GetSelectEntry())
-
-		// paset entry
-		case event.Rune() == 'p':
-			//for _, source := range gui.Register.MoveSources {
-			//	dest := filepath.Join(gui.InputPath.GetText(), filepath.Base(source))
-			//	if err := os.Rename(source, dest); err != nil {
-			//		log.Printf("cannot copy or move the file: %s", err)
-			//	}
-			//}
-
-			// TODO implement file copy
-			//for _, source := range gui.Register.CopyResources {
-			//dest := filepath.Join(gui.InputPath.GetText(), filepath.Base(source))
-			//}
-
-			//gui.EntryManager.SetEntries(gui.InputPath.GetText())
-
-		// edit file with $EDITOR
-		case event.Rune() == 'e':
-			editor := os.Getenv("EDITOR")
-			if editor == "" {
-				log.Println("$EDITOR is empty, please set $EDITOR")
-				return event
-			}
-
-			entry := gui.EntryManager.GetSelectEntry()
-
-			gui.App.Suspend(func() {
-				if err := gui.ExecCmd(true, editor, entry.PathName); err != nil {
-					log.Printf("%s: %s\n", ErrEdit, err)
-				}
-			})
-		case event.Rune() == 'q':
-			gui.Stop()
-		}
-
-		return event
-	})
-
-	gui.EntryManager.SetSelectionChangedFunc(func(row, col int) {
-		if row > 0 {
-			f := gui.EntryManager.Entries()[row-1]
-			gui.Preview.UpdateView(gui, f)
-		}
-	})
-
-	gui.InputPath.SetDoneFunc(func(key tcell.Key) {
-		if key == tcell.KeyEscape {
-			gui.App.Stop()
-		}
-
-		if key == tcell.KeyEnter {
-			path := gui.InputPath.GetText()
-			path = os.ExpandEnv(path)
-			gui.InputPath.SetText(path)
-			row, _ := gui.EntryManager.GetSelection()
-			gui.HistoryManager.Save(row, path)
-			gui.EntryManager.SetEntries(path)
-		}
-
-	}).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyTab {
-			gui.App.SetFocus(gui.EntryManager)
-		}
-
-		return event
-	})
+	gui.SetKeybindings()
 
 	grid := tview.NewGrid().SetRows(1, 0).SetColumns(0, 0)
 	grid.AddItem(gui.InputPath, 0, 0, 1, 2, 0, 0, true).
--- gui/keybindings.go
+++ gui/keybindings.go
@@ -1,13 +1,20 @@
 package gui
 
 import (
+	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/gdamore/tcell"
 )
 
+func (gui *Gui) SetKeybindings() {
+	gui.InputPathKeybinding()
+	gui.EntryManagerKeybinding()
+}
+
 // globalKeybinding
-func globalKeybinding(gui *Gui, event *tcell.EventKey) {
+func (gui *Gui) GlobalKeybinding(event *tcell.EventKey) {
 	switch {
 	// go to input view
 	case event.Key() == tcell.KeyTab:
@@ -63,5 +70,99 @@ func globalKeybinding(gui *Gui, event *tcell.EventKey) {
 			gui.Preview.UpdateView(gui, entry)
 		}
 	}
+}
 
+func (gui *Gui) EntryManagerKeybinding() {
+	gui.EntryManager.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			gui.App.Stop()
+		}
+
+	}).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		gui.GlobalKeybinding(event)
+		switch {
+		// cut entry
+		case event.Rune() == 'd':
+			if !hasEntry(gui) {
+				return event
+			}
+			//gui.EntryManager.SetViewable(false)
+			gui.Register.MoveSources = append(gui.Register.MoveSources, gui.EntryManager.GetSelectEntry())
+
+		// copy entry
+		case event.Rune() == 'y':
+			if !hasEntry(gui) {
+				return event
+			}
+			gui.Register.CopySources = append(gui.Register.CopySources, gui.EntryManager.GetSelectEntry())
+
+		// paset entry
+		case event.Rune() == 'p':
+			//for _, source := range gui.Register.MoveSources {
+			//	dest := filepath.Join(gui.InputPath.GetText(), filepath.Base(source))
+			//	if err := os.Rename(source, dest); err != nil {
+			//		log.Printf("cannot copy or move the file: %s", err)
+			//	}
+			//}
+
+			// TODO implement file copy
+			//for _, source := range gui.Register.CopyResources {
+			//dest := filepath.Join(gui.InputPath.GetText(), filepath.Base(source))
+			//}
+
+			//gui.EntryManager.SetEntries(gui.InputPath.GetText())
+
+		// edit file with $EDITOR
+		case event.Rune() == 'e':
+			editor := os.Getenv("EDITOR")
+			if editor == "" {
+				log.Println("$EDITOR is empty, please set $EDITOR")
+				return event
+			}
+
+			entry := gui.EntryManager.GetSelectEntry()
+
+			gui.App.Suspend(func() {
+				if err := gui.ExecCmd(true, editor, entry.PathName); err != nil {
+					log.Printf("%s: %s\n", ErrEdit, err)
+				}
+			})
+		case event.Rune() == 'q':
+			gui.Stop()
+		}
+
+		return event
+	})
+
+	gui.EntryManager.SetSelectionChangedFunc(func(row, col int) {
+		if row > 0 {
+			f := gui.EntryManager.Entries()[row-1]
+			gui.Preview.UpdateView(gui, f)
+		}
+	})
+
+}
+
+func (gui *Gui) InputPathKeybinding() {
+	gui.InputPath.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			gui.App.Stop()
+		}
+
+		if key == tcell.KeyEnter {
+			path := gui.InputPath.GetText()
+			path = os.ExpandEnv(path)
+			gui.InputPath.SetText(path)
+			row, _ := gui.EntryManager.GetSelection()
+			gui.HistoryManager.Save(row, path)
+			gui.EntryManager.SetEntries(path)
+		}
+
+	}).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyTab {
+			gui.App.SetFocus(gui.EntryManager)
+		}
+
+		return event
+	})
 }
