e6fbaf29ab16932ea6e24be741b255d162beeeb0
skanehira <sho19921005@gmail.com>
2019-11-15 00:59:46 +0900

    check if file exists

--- system/file.go
+++ system/file.go
@@ -1,11 +1,21 @@
 package system
 
 import (
+	"errors"
 	"io"
 	"os"
 )
 
+var (
+	ErrFileExist = errors.New("file already exist")
+)
+
 func CopyFile(src, target string) error {
+	_, err := os.Stat(src)
+	if !os.IsExist(err) {
+		return ErrFileExist
+	}
+
 	s, err := os.Open(src)
 	if err != nil {
 		return err
