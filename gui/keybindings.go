7bcbd6e1fc71b25fa45ca2d317a2a1145f26c75a
skanehira <sho19921005@gmail.com>
2019-11-19 07:01:06 +0900

    improve cursor position when move directories

--- gui/keybindings.go
+++ gui/keybindings.go
@@ -31,22 +31,22 @@ func (gui *Gui) GlobalKeybinding(event *tcell.EventKey) {
 		gui.App.SetFocus(gui.InputPath)
 
 	// go to previous history
-	case event.Key() == tcell.KeyCtrlH:
-		history := gui.HistoryManager.Previous()
-		if history != nil {
-			gui.InputPath.SetText(history.Path)
-			gui.EntryManager.SetEntries(history.Path)
-			gui.EntryManager.Select(history.RowIdx, 0)
-		}
+	//case event.Key() == tcell.KeyCtrlH:
+	//	history := gui.HistoryManager.Previous()
+	//	if history != nil {
+	//		gui.InputPath.SetText(history.Path)
+	//		gui.EntryManager.SetEntries(history.Path)
+	//		gui.EntryManager.Select(history.RowIdx, 0)
+	//	}
 
-	// go to next history
-	case event.Key() == tcell.KeyCtrlL:
-		history := gui.HistoryManager.Next()
-		if history != nil {
-			gui.InputPath.SetText(history.Path)
-			gui.EntryManager.SetEntries(history.Path)
-			gui.EntryManager.Select(history.RowIdx, 0)
-		}
+	//// go to next history
+	//case event.Key() == tcell.KeyCtrlL:
+	//	history := gui.HistoryManager.Next()
+	//	if history != nil {
+	//		gui.InputPath.SetText(history.Path)
+	//		gui.EntryManager.SetEntries(history.Path)
+	//		gui.EntryManager.Select(history.RowIdx, 0)
+	//	}
 
 	// go to previous dir
 	case event.Rune() == 'h':
@@ -55,7 +55,6 @@ func (gui *Gui) GlobalKeybinding(event *tcell.EventKey) {
 		if path != "" {
 			gui.InputPath.SetText(path)
 			gui.EntryManager.SetEntries(path)
-			gui.EntryManager.Select(1, 0)
 			gui.EntryManager.SetOffset(0, 0)
 			entry = gui.EntryManager.GetSelectEntry()
 			gui.Preview.UpdateView(gui, entry)
@@ -71,8 +70,13 @@ func (gui *Gui) GlobalKeybinding(event *tcell.EventKey) {
 				gui.EntryManager.SetEntries(entry.PathName)
 				gui.HistoryManager.Save(row, filepath.Join(filepath.Dir(gui.InputPath.GetText()), entry.Path))
 				gui.InputPath.SetText(entry.PathName)
-				gui.EntryManager.Select(1, 0)
-				gui.EntryManager.SetOffset(1, 0)
+
+				count := gui.EntryManager.GetRowCount()
+				if row > count {
+					gui.EntryManager.Select(count-1, 0)
+					gui.EntryManager.Select(0, 0)
+				}
+
 				entry := gui.EntryManager.GetSelectEntry()
 				gui.Preview.UpdateView(gui, entry)
 			}
